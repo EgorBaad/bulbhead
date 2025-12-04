97f8eb10c5a4844a721fee9e571555eda65ee0f7
EgorBaad <bd412997@gmail.com>
2025-12-04 17:00:44 +0300

    Added idle animation and trigger

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -8,12 +8,25 @@ public partial class Player : CharacterBody2D
 	/// </summary>
 	[Export]
 	public float Speed = 100.0f;
+	/// <summary>
+    /// Timer to use for idle animation triggering.
+    /// </summary>
+	[Export]
+	public Timer IdleTimer;
 
 	/// <summary>
 	/// Signal emitted on interaction input, while the animation plays.
 	/// </summary>
 	[Signal]
 	public delegate void InteractionEventHandler();
+
+	private AnimatedSprite2D _animation;
+
+	public override void _Ready()
+	{
+		_animation = GetNode<AnimatedSprite2D>("PlayerAnimation");
+		IdleTimer.Timeout += HandleIdleTimeout;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -39,6 +52,7 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		AnimateMovement();
+		CheckIdle();
 		MoveAndSlide();
 	}
 
@@ -50,40 +64,53 @@ public partial class Player : CharacterBody2D
 	/// </remarks>
 	private void AnimateMovement()
 	{
-		AnimatedSprite2D animatedSprite = GetNode<AnimatedSprite2D>("PlayerAnimation");
 		if (Velocity.X != 0)
 		{
-			animatedSprite.Animation = "walk_side";
+			_animation.Animation = "walk_side";
 			if (Velocity.X > 0)
 			{
-				animatedSprite.FlipH = true;
+				_animation.FlipH = true;
 			}
 			else
 			{
-				animatedSprite.FlipH = false;
+				_animation.FlipH = false;
 			}
 		}
 		else if (Velocity.Y != 0)
 		{
 			if (Velocity.Y > 0)
 			{
-				animatedSprite.Animation = "walk_down";
+				_animation.Animation = "walk_down";
 			}
 			else
 			{
-				animatedSprite.Animation = "walk_up";
+				_animation.Animation = "walk_up";
 			}
 		}
 		else
 		{
-			if (animatedSprite.IsPlaying() && animatedSprite.Animation.Equals("interact"))
+			if (!_animation.IsPlaying() || _animation.Animation.ToString().StartsWith("walk_"))
 			{
-				return;
+				_animation.Animation = "default";
 			}
-			animatedSprite.Animation = "default";
 		}
 		
-		animatedSprite.Play();
+		_animation.Play();
+	}
+
+	/// <summary>
+	/// Checks if the player is idle and starts/stops the idle timer accordingly.
+	/// </summary>
+	private void CheckIdle()
+	{
+		if (_animation.Animation == "default" && IdleTimer.IsStopped())
+		{
+			IdleTimer.Start();
+		}
+		else if (_animation.Animation != "default" && !IdleTimer.IsStopped())
+		{
+			IdleTimer.Stop();
+		}
 	}
 
 	/// <summary>
@@ -95,5 +122,14 @@ public partial class Player : CharacterBody2D
 
 		AnimatedSprite2D animatedSprite = GetNode<AnimatedSprite2D>("PlayerAnimation");
 		animatedSprite.Animation = "interact";
+	}
+
+	/// <summary>
+    /// Actions to perform on idle state change.
+    /// </summary>
+	private void HandleIdleTimeout()
+	{
+		_animation.Animation = "idle";
+		_animation.Play();
 	}
 }
