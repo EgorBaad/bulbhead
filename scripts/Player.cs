d209d7a86ed584491284cd45a3419c74ff7b024c
EgorBaad <bd412997@gmail.com>
2025-12-04 15:27:55 +0300

    Movement and interaction implemented

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,38 +3,97 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	/// <summary>
+	/// The speed at which the player moves. Sets to velocity.
+	/// </summary>
+	[Export]
+	public float Speed = 100.0f;
+
+	/// <summary>
+	/// Signal emitted on interaction input, while the animation plays.
+	/// </summary>
+	[Signal]
+	public delegate void InteractionEventHandler();
 
 	public override void _PhysicsProcess(double delta)
 	{
+		if (Input.IsActionJustPressed("interact"))
+		{
+			HandleInteraction();
+			return;
+		}
+
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity = direction * Speed;
+
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity = Vector2.Zero;
 		}
 
 		Velocity = velocity;
+		AnimateMovement();
 		MoveAndSlide();
+	}
+
+	/// <summary>
+	/// Animates the player based on movement direction.
+	/// </summary>
+	/// <remarks>
+	/// Takes the current velocity and sets the appropriate animation.
+	/// </remarks>
+	private void AnimateMovement()
+	{
+		AnimatedSprite2D animatedSprite = GetNode<AnimatedSprite2D>("PlayerAnimation");
+		if (Velocity.X != 0)
+		{
+			animatedSprite.Animation = "walk_side";
+			if (Velocity.X > 0)
+			{
+				animatedSprite.FlipH = true;
+			}
+			else
+			{
+				animatedSprite.FlipH = false;
+			}
+		}
+		else if (Velocity.Y != 0)
+		{
+			if (Velocity.Y > 0)
+			{
+				animatedSprite.Animation = "walk_down";
+			}
+			else
+			{
+				animatedSprite.Animation = "walk_up";
+			}
+		}
+		else
+		{
+			if (animatedSprite.IsPlaying() && animatedSprite.Animation.Equals("interact"))
+			{
+				return;
+			}
+			animatedSprite.Animation = "default";
+		}
+		
+		animatedSprite.Play();
+	}
+
+	/// <summary>
+	/// Handles player interaction input. Emits the Interaction signal.
+	/// </summary>
+	private void HandleInteraction()
+	{
+		EmitSignal(SignalName.Interaction);
+
+		AnimatedSprite2D animatedSprite = GetNode<AnimatedSprite2D>("PlayerAnimation");
+		animatedSprite.Animation = "interact";
 	}
 }
